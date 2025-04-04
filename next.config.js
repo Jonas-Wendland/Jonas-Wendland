82be871a6b774368f5bfebf4ed24f9660d0bd48c
Jonas-Wendland <wendjon@gmx.de>
2025-04-04 19:36:01 +0200

    cleaning

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/Jonas-Wendland',
-    assetPrefix: '/Jonas-Wendland',
-  };
-  
-  module.exports = nextConfig;
-  
