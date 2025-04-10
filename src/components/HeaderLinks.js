219dd9883c70cc30c992defed9e0b73a73103b59
Jonas-Wendland <wendjon@gmx.de>
2025-04-10 10:06:03 +0200

    GitHub

---
+++ src/components/HeaderLinks.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Icons from "./svgicon";
+import GithubIcon from "../lib/SVGIcons/GithubIcon";
+
+
+export default function HeaderLinks() {
+    return (<>
+ 
+    <Styleda href="https://github.com/Jonas-Wendland" target="_blank" rel="noopener noreferrer">
+        <Icons icon={GithubIcon}/>
+        <p>Jonas-Wendland</p>
+        </Styleda>
+    </>
+    )
+}
+
+const Styleda = styled.a`
+    color: white;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 1rem;
+    font-weight: bold;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    border-radius: 2rem;
+    background-color: rgb(0, 223, 67);
+    transition: background-color 0.3s ease-in-out, transform 0.3s ease-in-out; 
+    
+    &:hover {
+        background-color: rgb(137, 240, 183);
+        transform: scale(1.05);
+        cursor: pointer;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+      }
+`;
