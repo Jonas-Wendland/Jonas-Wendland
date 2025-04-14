8588f52d7b4f60303fbe0e46bdb3a781733b8a8e
Jonas-Wendland <wendjon@gmx.de>
2025-04-14 13:00:26 +0200

    skillscard change

--- src/components/skillsCard.js
+++ src/components/skillsCard.js
@@ -18,8 +18,9 @@ justify-content: space-evenly;
 align-items: center;
 border: 2px solid black;
 border-radius: 2rem;
-height: 10rem;
-width: 10rem;
+min-height: 9rem;
+min-width: 9rem;
+max-width: 9rem;
 box-shadow: 3px 3px 3px black;
 
 `
